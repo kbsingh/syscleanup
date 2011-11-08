d6702f26ab6baffdb8685da9d9792a6cddb77815
Karanbir Singh <kbsingh@karan.org>
2011-11-08 15:10:01 +0000

    add a note about running as root; GPL the script

--- useage_age.sh
+++ useage_age.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
-
-# Copyright Karanbir Singh <kbsingh@karan.org>, Nov 2011
-# 
+#
+# Copyright Karanbir Singh, kbsingh@karan.org, http://www.karan.org/ ;Nov 2011
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ---------------------------------------------------------------------------
 # Util to help locate rpms on a machine which might not
-# be used at all.
-
+# be used at all. Might want to run this as the root user.
+#
 # XXX: carefully inspect the output, some rpms provide
 # placeholder content, which is needed but not used
 # eg: filesystem 
-
+#
+# ---------------------------------------------------------------------------
 
 tst_dir=$(mktemp -d)
 
